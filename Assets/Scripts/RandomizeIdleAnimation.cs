c8b348df7793cff05bed99423bb39854d24d2816
Aimar <ivan.harankou@gmail.com>
2026-04-30 03:06:38 +0300

    Env and characters setup wip

---
+++ Assets/Scripts/RandomizeIdleAnimation.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RandomizeIdleAnimation : MonoBehaviour
+{
+    [SerializeField] private string animationStateName = "Idle";
+    [SerializeField] private Animator animator;
+
+    private void Start()
+    {
+        if (animator == null) return;
+
+        animator.Play(animationStateName, 0, Random.value);
+    }
+}
